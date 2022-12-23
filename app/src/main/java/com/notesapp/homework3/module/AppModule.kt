4aa8910b2b3808518c1a255b831049065a55dc9c
bumbumapp <bumbumapps@gmail.com>
2022-12-23 14:56:15 -0800

    Notes app with Hilt,RoomDatabase

--- app/src/main/java/com/notesapp/homework3/module/AppModule.kt
+++ app/src/main/java/com/notesapp/homework3/module/AppModule.kt
@@ -10,6 +10,19 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+/* @Module : It is used to create an object for any component dependency,
+mostly used for the third party or where constructor and field injection cannot be performed.
+Tell hilt that this is a module.
+
+@InstallIn- Installin tells the container where the bindings are.
+@Installn (SingletonComponent::class) helps activity component class’s dependency to inject via hilt.
+ This annotation means that all of the dependencies in this Module are available in all of the app’s activities.
+ @Singleton- It will create a singleton instance for a dependency,
+  which will be shared across the application.
+  This makes the application container always provide the same instance regardless of whether the type
+   of dependencies we are using is the same or a different one.
+
+ */
 
 @Module
 @InstallIn(SingletonComponent::class)
